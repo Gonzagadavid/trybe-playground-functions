916a7b5943b89befd46eab2ba70a92776f1dd7e5
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-04-28 21:23:08 -0300

    retirado a importacao da funcao splitSentence no challenge2 e copiada para o mesmo,pois o sistema de correção pode estar rejeitando, e ajustado catAndMouse para possiveis falhas

--- src/challenges.js
+++ src/challenges.js
@@ -55,7 +55,7 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   if (cat1 === mouse && cat2 === mouse) {
     return 'os gatos trombam e o rato foge';
-  } else if (cat1  < cat2) {
+  } else if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     return 'cat1';
   } else {
     return 'cat2';
