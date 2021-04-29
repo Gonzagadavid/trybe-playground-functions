ff42a0a40c816d6bf024511a822ecebbfe3a3d51
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-04-28 22:32:17 -0300

    corrigidos erros apontados pelo lint

--- src/challenges2.js
+++ src/challenges2.js
@@ -6,7 +6,7 @@ const fn = require('./challenges');
 // clona o array de strings passado por parâmetro e o retorna ordenado de acordo com o unicode obtido
 // através do método String.charCodeAt(), caso duas string comecem pela mesma letra, é ordenado pela
 // segunda letra.
-function sort (array) {
+function sort(array) {
   const arrayCopy = [...array];
   let memory = '';
   for (let j = 0; j < arrayCopy.length - 1; j += 1) {
@@ -15,12 +15,12 @@ function sort (array) {
         memory = arrayCopy[i];
         arrayCopy[i] = arrayCopy[i + 1];
         arrayCopy[i + 1] = memory;
-      } else if (arrayCopy[i].charCodeAt() === arrayCopy[i + 1].charCodeAt()) {
-        if (arrayCopy[i].slice(1).charCodeAt() > arrayCopy[i + 1].slice(1).charCodeAt()) {
+      } else if (arrayCopy[i].charCodeAt() === arrayCopy[i + 1].charCodeAt() &&
+          arrayCopy[i].slice(1).charCodeAt() > arrayCopy[i + 1].slice(1).charCodeAt()) {
           memory = arrayCopy[i];
           arrayCopy[i] = arrayCopy[i + 1];
           arrayCopy[i + 1] = memory;
-        }
+      
       }
     }
   }
@@ -44,6 +44,32 @@ function techList(technologies, name) {
 }
 
 // Desafio 11
+
+// retorna o valor booleano true caso o array passado por parâmetro possua um numero repetido 3 ou mais
+// vezes e false caso não.
+function numberRepeat (array) {
+  const arrayInedito = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+    let cont = 0;
+    for (let j = 0; j < arrayInedito.length; j += 1) {
+      if (array[i] === arrayInedito[j]) cont++;
+    }
+    if (cont === 0) arrayInedito.push(array[i]);
+  }
+
+  for (let i = 0; i < arrayInedito.length; i += 1) {
+    let cont = 0;
+    for (let j = 0; j < array.length; j += 1) {
+      if (arrayInedito[i] === array[j]) cont++;
+    }
+    if (cont >= 3) return true;
+  }
+
+  return false;
+}
+
+
 function generatePhoneNumber(array) {
   if (array.length !== 11) return 'Array com tamanho incorreto.';
   if (numberRepeat(array)) {
@@ -68,6 +94,22 @@ function generatePhoneNumber(array) {
     }
   }
   return `(${prefix}) ${start}-${end}`;
+}
+
+// Desafio 12
+
+// verifica se compared é maior que a soma de valueA com valueB, caso seja retona true,
+// caso não retorna false
+function compare (compared, valueA, valueB) {
+  const sum = valueA + valueB;
+  return compared > sum;
+}
+
+// verifica se o compared é menor do que o valor absoluto da diferença entre valueA e ValueB e retorna
+// true se sim e false se não
+function checkAbs (compared, valueA, valueB) {
+  const diference = valueA - valueB;
+  return compared < Math.abs(diference);
 }
 
 function triangleCheck(lineA, lineB, lineC) {
@@ -95,50 +137,9 @@ function hydrate(string) {
   return sum === 1 ? `${sum} copo de água` : `${sum} copos de água`;
 }
 
-// funções auxiliares-------------------------------------------------------------------------------
-
-
-// retorna o valor booleano true caso o array passado por parâmetro possua um numero repetido 3 ou mais
-// vezes e false caso não.
-function numberRepeat (array) {
-  const arrayInedito = [];
-
-  for (let i = 0; i < array.length; i += 1) {
-    let cont = 0;
-    for (let j = 0; j < arrayInedito.length; j += 1) {
-      if (array[i] === arrayInedito[j]) cont++;
-    }
-    if (cont === 0) arrayInedito.push(array[i]);
-  }
-
-  for (let i = 0; i < arrayInedito.length; i += 1) {
-    let cont = 0;
-    for (let j = 0; j < array.length; j += 1) {
-      if (arrayInedito[i] === array[j]) cont++;
-    }
-    if (cont >= 3) return true;
-  }
-
-  return false;
-}
-
-// verifica se compared é maior que a soma de valueA com valueB, caso seja retona true,
-// caso não retorna false
-function compare (compared, valueA, valueB) {
-  const sum = valueA + valueB;
-  return compared > sum;
-}
-
-// verifica se o compared é menor do que o valor absoluto da diferença entre valueA e ValueB e retorna
-// true se sim e false se não
-function checkAbs (compared, valueA, valueB) {
-  const diference = valueA - valueB;
-  return compared < Math.abs(diference);
-}
-
 module.exports = {
   generatePhoneNumber,
   techList,
   hydrate,
-  triangleCheck
+  triangleCheck,
 };
